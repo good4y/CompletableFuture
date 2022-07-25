77b7b3aea7aaf77cc1629bcc5564e97915ae6482
kimyoungchul <>
2022-07-25 18:52:09 +0900

    Completablefuture 초기 세팅

---
+++ src/test/java/hanghae/talk/async/AsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghae.talk.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
